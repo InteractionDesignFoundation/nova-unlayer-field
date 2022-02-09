164f19cf0f277f20f0c1aa28b7d35a875c138b2e
Hasan Wajahat <hasan_wajahat@hotmail.com>
2022-02-09 10:55:52 +0500

    Use ES6 import to load vue component With latest latest mix require causes problem when loaded like this.

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,6 +1,11 @@
-Nova.booting((Vue, router, store) => {
-  Vue.component('unlayer-editor', require('./components/UnlayerEditor'))
-  Vue.component('index-nova-unlayer-field', require('./components/IndexField'))
-  Vue.component('detail-nova-unlayer-field', require('./components/DetailField'))
-  Vue.component('form-nova-unlayer-field', require('./components/FormField'))
-})
+import UnlayerEditor from './components/UnlayerEditor.vue';
+import IndexField from './components/IndexField.vue';
+import DetailField from './components/DetailField.vue';
+import FormField from './components/FormField.vue';
+
+Nova.booting((Vue) => {
+  Vue.component('unlayer-editor', UnlayerEditor);
+  Vue.component('index-nova-unlayer-field', IndexField);
+  Vue.component('detail-nova-unlayer-field', DetailField);
+  Vue.component('form-nova-unlayer-field', FormField);
+});
