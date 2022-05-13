61d9d647d8a4aefcd50a74963d9d3609eef5bd11
Alies Lapatsin <web.lapateen@gmail.com>
2022-05-13 18:17:18 +0400

    Cleanup

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,12 +1,13 @@
+/* global Nova */
 import UnlayerEditor from './components/UnlayerEditor.vue';
 import IndexField from './components/IndexField.vue';
 import DetailField from './components/DetailField.vue';
 import FormField from './components/FormField.vue';
 
 Nova.booting((app, store) => {
-  app.component('unlayer-editor', UnlayerEditor);
+    app.component('unlayer-editor', UnlayerEditor);
 
-  app.component('index-nova-unlayer-field', IndexField);
-  app.component('detail-nova-unlayer-field', DetailField);
-  app.component('form-nova-unlayer-field', FormField);
+    app.component('index-nova-unlayer-field', IndexField);
+    app.component('detail-nova-unlayer-field', DetailField);
+    app.component('form-nova-unlayer-field', FormField);
 })
